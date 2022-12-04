5fdc0407ec1aade3a0f15bf926e0b0773de26175
Doug <70054506+smykad@users.noreply.github.com>
2022-12-04 04:38:29 -0500

    pagination, sort, order, group

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using EmployeesCh12.Data;
 using EmployeesCh12.Models;
+using EmployeesCh12.ViewModels;
 
 namespace EmployeesCh12.Controllers
 {
@@ -18,12 +19,63 @@ namespace EmployeesCh12.Controllers
         {
             _context = context;
         }
-
-        // GET: Employee
-        public async Task<IActionResult> Index()
+        public IActionResult DeptCount()
         {
-            var employeeContext = _context.Employees.Include(e => e.Department);
-            return View(await employeeContext.ToListAsync());
+            IQueryable<DepartmentGroup> data =
+                 from employee in _context.Employees.Include(e => e.Department)
+                 group employee by employee.DepartmentID into deptGroup
+                 select new DepartmentGroup()
+                 {
+                     DepartmentID = deptGroup.Key,
+                     DepartmentCount = deptGroup.Count()
+                 };
+            return View(data.ToList());
+        }
+        // GET: Employee
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? pageNumber)
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+
+            var employees = from e in _context.Employees.Include(e => e.Department).Include(e => e.Benefits)
+                           select e;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                employees = employees.Where(s => s.LastName.Contains(searchString)
+                                        || s.FirstName.Contains(searchString)); 
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    employees = employees.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    employees = employees.OrderBy(s => s.HireDate);
+                    break;
+                case "date_desc":
+                    employees = employees.OrderByDescending(s => s.HireDate);
+                    break;
+                default:
+                    employees = employees.OrderBy(s => s.LastName);
+                    break;
+            }
+            int pageSize = 3;
+            return View(await PaginatedList<Employee>.CreateAsync(employees.AsNoTracking(), pageNumber ?? 1, pageSize));
+
         }
 
         // GET: Employee/Details/5
@@ -156,5 +208,7 @@ namespace EmployeesCh12.Controllers
         {
             return _context.Employees.Any(e => e.ID == id);
         }
+
+
     }
 }
---
+++ ViewModels/DepartmentGroup.cs
@@ -0,0 +1,9 @@
+﻿using EmployeesCh12.Models;
+namespace EmployeesCh12.ViewModels
+{
+    public class DepartmentGroup
+    {
+        public int DepartmentID { get; set; }
+        public int DepartmentCount { get; set; }
+    }
+}
